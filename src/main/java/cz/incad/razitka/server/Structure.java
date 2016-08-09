676c08f719752450b33abda811debd5149323f45
vlahoda <vlahoda@gmail.com>
2016-08-09 17:31:19 +0200

    switched to gradle

--- src/main/java/cz/incad/razitka/server/Structure.java
+++ src/main/java/cz/incad/razitka/server/Structure.java
@@ -1,21 +1,27 @@
 package cz.incad.razitka.server;
 
+import javax.servlet.ServletException;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.aplikator.client.shared.descriptor.ApplicationDTO;
 import org.aplikator.server.Configurator;
 import org.aplikator.server.data.Context;
 import org.aplikator.server.descriptor.Application;
+import org.aplikator.server.descriptor.Function;
 import org.aplikator.server.descriptor.Menu;
 
 import com.typesafe.config.ConfigValue;
 
 import cz.incad.razitka.DLists;
 import cz.incad.razitka.Exemplar;
+import cz.incad.razitka.ImportRazitek;
 
 
 public class Structure extends Application {
+    private static final Logger LOG = Logger.getLogger(Structure.class.getName());
 
     public static final DLists DLists = new DLists();
     public static final Exemplar Exemplar = new Exemplar();
@@ -51,5 +57,26 @@ public class Structure extends Application {
         }
         retval.getConfig().putAll(Configurator.get().getSystemLabels(ctx.getUserLocale()));
         return retval;
+    }
+
+    @Override
+    public void initialize()  {
+        try {
+            LOG.info("Razitka Loader started");
+
+            Menu menuAgendy = new Menu("agendy");
+            menuAgendy.addView(Structure.Exemplar.view());
+
+            Menu menuAdministrace = new Menu("administrace");
+            menuAdministrace.addView(Structure.DLists.druh());
+            Function importFunction = new Function("ImportRazitek", "ImportRazitek", new ImportRazitek());
+            menuAdministrace.addFunction(importFunction);
+
+            addMenu(menuAgendy).addMenu(menuAdministrace);
+            LOG.info("Razitka Loader finished");
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Razitka Loader error:", ex);
+            throw new RuntimeException("Razitka Loader error: ", ex);
+        }
     }
 }
