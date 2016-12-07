e0bb1c8bce96f6fd551c9d066c948f065c8c48f2
vlahoda <vlahoda@gmail.com>
2016-12-07 11:22:30 +0100

    export support

--- src/main/java/cz/incad/razitka/ExportCSV.java
+++ src/main/java/cz/incad/razitka/ExportCSV.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 
 import org.aplikator.client.shared.data.ClientContext;
 import org.aplikator.client.shared.data.FunctionResult;
+import org.aplikator.client.shared.data.FunctionResultType;
 import org.aplikator.client.shared.descriptor.QueryDescriptorDTO;
 import org.aplikator.client.shared.descriptor.QueryParameterDTO;
 import org.aplikator.server.DescriptorRegistry;
@@ -33,13 +34,17 @@ import cz.incad.razitka.server.Structure;
 
 public class ExportCSV extends Function {
     private static final String NAME = "ExportCSV";
-    Property<Integer> start;
-    Property<Integer> count;
+    Property<String> filename;
 
 
 
     public ExportCSV() {
         super(NAME, NAME, new Executor());
+        WizardPage p1 = new WizardPage(this, "firstPage");
+        filename = p1.stringProperty("filename");
+        p1.form(row(
+                column(filename)
+        ), false);
 
     }
 
@@ -73,26 +78,26 @@ public class ExportCSV extends Function {
 
 
 
-                StringBuilder sb = new StringBuilder("id,"+
-                        "signatura," +
-                        "sys," +
-                        "napis," +
-                        "druh," +
-                        "prijmeni," +
-                        "instituce," +
-                        "obecne," +
-                        "mesto\r\n");
+                StringBuilder sb = new StringBuilder("\"ID\","+
+                        "\"Nápis\"," +
+                        "\"Signatura\"," +
+                        "\"Sys\"," +
+                        "\"Druh\"," +
+                        "\"Osoba\"," +
+                        "\"Instituce\"," +
+                        "\"Obecné poznámky\"," +
+                        "\"Město\"\r\n");
 
-                logger.info("STARTED READING RECORDS");
+                logger.info("STARTED READING RECORDS ");
                 for (Record slozka : context.getRecords(Structure.Exemplar)
                         .withQuery(queryExpression)
                         .withSort(sortItems)
                         .list()) {
                     logger.info("RECORD:"+slozka.getPrimaryKey().getId()+" - "+slozka.getStringValue(Structure.Exemplar.napis, context));
                     sb.append("\"").append(slozka.getPrimaryKey().getId()).append("\"").append(",");
-                    sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.signatura, context)/*.replaceAll("\n","\\n")*/).append("\",");
-                    sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.sys, context)/*.replaceAll("\n","\\n")*/).append("\",");
                     sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.napis, context)).append("\",");
+                    sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.signatura, context)).append("\",");
+                    sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.sys, context)).append("\",");
                     sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.druh, context)).append("\",");
                     sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.prijmeni, context)).append("\",");
                     sb.append("\"").append(slozka.getStringValue(Structure.Exemplar.instituce, context)).append("\",");
@@ -104,11 +109,11 @@ public class ExportCSV extends Function {
                 logger.info("FINISHED READING RECORDS");
 
                 Tempstore ts = TempstoreFactory.getTempstore();
-                String fileTempID = ts.store("export.csv", new ByteArrayInputStream(sb.toString().getBytes("UTF-8")), false);
+                String fileTempID = ts.store(wizardParameters.getValue(func.filename), new ByteArrayInputStream(sb.toString().getBytes("UTF-8")), false);
                 HttpServletRequest req = context.getHttpServletRequest();
                 String baseUrl = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + req.getContextPath()
                         + "/export?fileId=" + fileTempID;
-                return new FunctionResult(baseUrl, true, true);
+                return new FunctionResult(baseUrl, true, FunctionResultType.DOWNLOAD);
             } catch (Throwable t) {
 
                 return new FunctionResult("Export do CSV nebyl spuštěn: " + t, false);
@@ -118,7 +123,11 @@ public class ExportCSV extends Function {
 
         @Override
         public WizardPage getWizardPage(String currentPageId, boolean forwardDirection, Record currentRecord, Record wizardParameters, ClientContext clientContext, Context context) {
-            return null;
+            WizardPage page = super.getWizardPage(currentPageId, forwardDirection, currentRecord, wizardParameters, clientContext, context);
+            ExportCSV func = (ExportCSV) function;
+            wizardParameters.setValue(func.filename, "export.csv");
+            page.setClientRecord(wizardParameters);
+            return page;
         }
     }
 
